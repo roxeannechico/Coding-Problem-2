74d499db070aa48638f7a6d5487bdd249f7f9fb9
roxeannechico <roseannechico22@gmail.com>
2024-11-04 21:24:28 +0800

    git commit -m "initial commit" git push origin main

---
+++ classSubject.js
@@ -0,0 +1,7 @@
+let subjectTitle = "Art Appreciation"
+let classSchedule = "9:30-11:00 pm in Tuesday and Wednesday"
+let classroom = "CHED 124"
+let classInstructor = "Mr. Michael Gem Marquez"
+let studentName = "Rose Anne D. Chico"
+
+console.log('{studentName} is currently enrolled in {subjectTitle} with a class schedule of {classSchedule} at room {classroom}. The instructor for the subject is {classInstructor}.')
